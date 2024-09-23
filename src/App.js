7ff12cfaed7645df3d42223cc7cbcc23b6f2b041
Sugatraj <sugatraj.2106@gmail.com>
2024-09-23 16:43:15 +0530

    added table_number from useParams

--- src/App.js
+++ src/App.js
@@ -1,94 +1,9 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import Signinscreen from "./screens/Signinscreen";
-import Signupscreen from "./screens/SignupScreen";
-import Verifyotp from "./screens/Verifyotp"; // Ensure Verifyotp is imported correctly
-import Profile from "./screens/Profile";
-import Wishlist from "./screens/Wishlist";
-import Category from "./screens/Category";
-import Cart from "./screens/Cart";
-import EditProfile from "./screens/EditProfile";
-import MyOrder from "./screens/MyOrder";
-import Product from "./screens/Product";
-import TrackOrder from "./screens/TrackOrder";
-import Search from "./screens/Search";
-import Checkout from "./screens/Checkout";
-import MenuDetails from "./screens/MenuDetails";
-import OrderTracking from "./screens/OrderTracking";
-
-import { RestaurantIdProvider } from "./context/RestaurantIdContext";
-import { CartProvider } from "./context/CartContext";
-
-
-function App() {
-  return (
-    <RestaurantIdProvider>
-      <CartProvider>
-        <Routes>
-          <Route path="/HomeScreen/:restaurantCode" element={<HomeScreen />} />
-          <Route
-            path="/HomeScreen"
-            element={<Navigate to="/HomeScreen/681316" replace />}
-          />
-          <Route
-            path="/"
-            element={<Navigate to="/HomeScreen/681316" replace />}
-          />
-          <Route path="/Signinscreen" element={<Signinscreen />} />
-          <Route path="/Signupscreen" element={<Signupscreen />} />
-          
-          <Route path="/Verifyotp" element={<Verifyotp />} />
-          {/* Use lowercase 'o' */}
-         
-          <Route path="/Wishlist" element={<Wishlist />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Checkout" element={<Checkout />} />
-          <Route path="/Category" element={<Category />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/EditProfile" element={<EditProfile />} />
-          <Route path="/MyOrder" element={<MyOrder />} />
-         
-          <Route path="/Menu" element={<Product />} />
-          <Route path="/Search" element={<Search />} />
-          
-          <Route path="/ProductDetails/:menuId" element={<MenuDetails />} />
-          <Route path="/TrackOrder/:order_number" element={<TrackOrder />} />
-          
-         
-        </Routes>
-      </CartProvider>
-    </RestaurantIdProvider>
-  );
-}
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import React from "react";
 // import { Routes, Route, Navigate } from "react-router-dom";
 // import HomeScreen from "./screens/HomeScreen";
 // import Signinscreen from "./screens/Signinscreen";
 // import Signupscreen from "./screens/SignupScreen";
-// import ForgotPassword from "./screens/Forgotpw";
-// import Verifyotp from "./screens/Verifyotp";
-// import Changepassword from "./screens/Changepassword";
+// import Verifyotp from "./screens/Verifyotp"; // Ensure Verifyotp is imported correctly
 // import Profile from "./screens/Profile";
 // import Wishlist from "./screens/Wishlist";
 // import Category from "./screens/Category";
@@ -97,16 +12,14 @@ export default App;
 // import MyOrder from "./screens/MyOrder";
 // import Product from "./screens/Product";
 // import TrackOrder from "./screens/TrackOrder";
-// import Review from "./screens/Review";
 // import Search from "./screens/Search";
 // import Checkout from "./screens/Checkout";
 // import MenuDetails from "./screens/MenuDetails";
 // import OrderTracking from "./screens/OrderTracking";
-// import Faq from "./screens/Faq";
-// import Test from "./component/Test";
 
 // import { RestaurantIdProvider } from "./context/RestaurantIdContext";
 // import { CartProvider } from "./context/CartContext";
+
 
 // function App() {
 //   return (
@@ -124,9 +37,10 @@ export default App;
 //           />
 //           <Route path="/Signinscreen" element={<Signinscreen />} />
 //           <Route path="/Signupscreen" element={<Signupscreen />} />
-//           <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          
 //           <Route path="/Verifyotp" element={<Verifyotp />} />
-//           <Route path="/Changepassword" element={<Changepassword />} />
+//           {/* Use lowercase 'o' */}
+         
 //           <Route path="/Wishlist" element={<Wishlist />} />
 //           <Route path="/Cart" element={<Cart />} />
 //           <Route path="/Checkout" element={<Checkout />} />
@@ -134,17 +48,14 @@ export default App;
 //           <Route path="/Profile" element={<Profile />} />
 //           <Route path="/EditProfile" element={<EditProfile />} />
 //           <Route path="/MyOrder" element={<MyOrder />} />
-//           <Route path="/Faq" element={<Faq />} />
-//           <Route path="/Product" element={<Product />} />
+         
+//           <Route path="/Menu" element={<Product />} />
 //           <Route path="/Search" element={<Search />} />
-//           <Route path="/OrderTracking" element={<OrderTracking />} />
-//           <Route path="/ProductDetails/:menuCatId" element={<MenuDetails />} />
+          
+//           <Route path="/ProductDetails/:menuId" element={<MenuDetails />} />
 //           <Route path="/TrackOrder/:order_number" element={<TrackOrder />} />
-//           <Route
-//             path="/Review/:order_number/:restaurantCode"
-//             element={<Review />}
-//           />
-//           <Route path="/testing" element={<Test />} />
+          
+         
 //         </Routes>
 //       </CartProvider>
 //     </RestaurantIdProvider>
@@ -152,3 +63,86 @@ export default App;
 // }
 
 // export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import Signinscreen from "./screens/Signinscreen";
+import Signupscreen from "./screens/SignupScreen";
+import Verifyotp from "./screens/Verifyotp";
+import Profile from "./screens/Profile";
+import Wishlist from "./screens/Wishlist";
+import Category from "./screens/Category";
+import Cart from "./screens/Cart";
+import EditProfile from "./screens/EditProfile";
+import MyOrder from "./screens/MyOrder";
+import Product from "./screens/Product";
+import TrackOrder from "./screens/TrackOrder";
+import Search from "./screens/Search";
+import Checkout from "./screens/Checkout";
+import MenuDetails from "./screens/MenuDetails";
+
+import { RestaurantIdProvider } from "./context/RestaurantIdContext";
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+  return (
+    <RestaurantIdProvider>
+      <CartProvider>
+        <Routes>
+          <Route
+            path="/HomeScreen/:restaurantCode/:table_number"
+            element={<HomeScreen />}
+          />{" "}
+          {/* Added table_number */}
+          <Route
+            path="/HomeScreen/:restaurantCode"
+            element={<Navigate to="/HomeScreen/681316/8" replace />}
+          />{" "}
+          {/* Adjusted for default */}
+          <Route
+            path="/HomeScreen"
+            element={<Navigate to="/HomeScreen/681316/8" replace />}
+          />{" "}
+          {/* Default route */}
+          <Route
+            path="/"
+            element={<Navigate to="/HomeScreen/681316/8" replace />}
+          />{" "}
+          {/* Redirect root to default */}
+          <Route path="/Signinscreen" element={<Signinscreen />} />
+          <Route path="/Signupscreen" element={<Signupscreen />} />
+          <Route path="/Verifyotp" element={<Verifyotp />} />
+          <Route path="/Wishlist" element={<Wishlist />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Category" element={<Category />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/EditProfile" element={<EditProfile />} />
+          <Route path="/MyOrder" element={<MyOrder />} />
+          <Route path="/Menu" element={<Product />} />
+          <Route path="/Search" element={<Search />} />
+          <Route path="/ProductDetails/:menuId" element={<MenuDetails />} />
+          <Route path="/TrackOrder/:order_number" element={<TrackOrder />} />
+        </Routes>
+      </CartProvider>
+    </RestaurantIdProvider>
+  );
+}
+
+export default App;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -76,6 +76,83 @@
 
 
 
+// import React, { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import "../assets/css/style.css";
+// import "../assets/css/custom.css";
+// import Sidebar from "../component/Sidebar";
+// import Bottom from "../component/bottom";
+// import OfferBanner from "../component/OfferBanner";
+// import ProductCart from "../component/ProductCart";
+// import { useRestaurantId } from "../context/RestaurantIdContext";
+
+// const HomeScreen = () => {
+//   const { restaurantCode } = useParams();
+//   const { setRestaurantCode, restaurantId, restaurantDetails } =
+//     useRestaurantId();
+
+//   useEffect(() => {
+//     if (restaurantCode) {
+//       console.log("Setting restaurant code:", restaurantCode);
+//       setRestaurantCode(restaurantCode);
+//     }
+//   }, [restaurantCode, setRestaurantCode]);
+
+//   useEffect(() => {
+//     // Update local storage whenever restaurantId or restaurantDetails changes
+//     if (restaurantId && restaurantDetails) {
+//       const userData = JSON.parse(localStorage.getItem("userData")) || {};
+//       userData.restaurantId = restaurantId; // Update restaurantId
+//       userData.restaurantName = restaurantDetails.name; // Update restaurant name
+//       localStorage.setItem("userData", JSON.stringify(userData)); // Update local storage
+//     }
+//   }, [restaurantId, restaurantDetails]);
+
+//   return (
+//     <div>
+//       <div className="page-wrapper">
+//         <Sidebar />
+
+//         <main className="page-content p-t100 p-b70">
+//           <div className="container overflow-hidden pt-0">
+//             <OfferBanner />
+//             <ProductCart />
+//           </div>
+//         </main>
+
+//         <Bottom />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomeScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../assets/css/style.css";
@@ -87,7 +164,7 @@ import ProductCart from "../component/ProductCart";
 import { useRestaurantId } from "../context/RestaurantIdContext";
 
 const HomeScreen = () => {
-  const { restaurantCode } = useParams();
+  const { restaurantCode, table_number } = useParams(); // Destructure table_number from useParams
   const { setRestaurantCode, restaurantId, restaurantDetails } =
     useRestaurantId();
 
@@ -99,14 +176,17 @@ const HomeScreen = () => {
   }, [restaurantCode, setRestaurantCode]);
 
   useEffect(() => {
-    // Update local storage whenever restaurantId or restaurantDetails changes
+    // Update local storage whenever restaurantId, restaurantDetails, or table_number changes
     if (restaurantId && restaurantDetails) {
       const userData = JSON.parse(localStorage.getItem("userData")) || {};
       userData.restaurantId = restaurantId; // Update restaurantId
       userData.restaurantName = restaurantDetails.name; // Update restaurant name
+      if (table_number) {
+        userData.tableNumber = table_number; // Store table_number
+      }
       localStorage.setItem("userData", JSON.stringify(userData)); // Update local storage
     }
-  }, [restaurantId, restaurantDetails]);
+  }, [restaurantId, restaurantDetails, table_number]); // Add table_number to the dependency array
 
   return (
     <div>
