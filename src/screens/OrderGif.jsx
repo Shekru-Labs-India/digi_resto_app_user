7b6383483ea7fee94bd0ce70e6fe28c895d3e850
Sugatraj <sugatraj.2106@gmail.com>
2024-09-30 17:30:49 +0530

    w

--- src/screens/OrderGif.jsx
+++ src/screens/OrderGif.jsx
@@ -3,7 +3,9 @@ import stewGif from '../assets/gif/stew.gif';
 
 const OrderGif = () => {
   return (
-    <img src={stewGif} alt="Cooking stew" style={{ width: '100%', height: 'auto' }} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+      <img src={stewGif} alt="Cooking stew" style={{ width: '100%', height: '70%' }} />
+    </div>
   );
 };
 
