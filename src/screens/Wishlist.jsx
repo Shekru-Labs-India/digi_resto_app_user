944da87dcf22d3df5a9acce5239d7c55263a21f7
Santosh-Mhetre <santoshmhetre608@gmail.com>
2024-10-01 10:44:00 +0530

    Fav Updated

--- src/screens/Wishlist.jsx
+++ src/screens/Wishlist.jsx
@@ -57,10 +57,7 @@ const Wishlist = () => {
       );
 
       if (response.ok) {
-        const updatedMenuList = [...menuList.map(menuList => ({
-          ...menuList,
-          oldPrice: Math.floor(menu.price * 1.1),
-        }))];
+        const updatedMenuList = [...menuList];
         updatedMenuList.splice(indexToRemove, 1);
         setMenuList(updatedMenuList);
       } else {
@@ -180,13 +177,9 @@ const Wishlist = () => {
     fetchFavoriteItems();
   }, [customerId, restaurantId]);
 
-  
-
   const handleRemoveItemClick = (index, menuId) => {
     removeItem(index, menuId);
   };
-
-
 
   return (
     <div className="page-wrapper full-height">
