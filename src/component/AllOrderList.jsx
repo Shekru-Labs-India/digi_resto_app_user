f652202c5e02cf64d32edfa3bd9b1e506cbe8769
Santosh-Mhetre <santoshmhetre608@gmail.com>
2024-10-26 16:01:55 +0530

    das

--- src/component/AllOrderList.jsx
+++ src/component/AllOrderList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import CompanyVersion from "../constants/CompanyVersion";
-import axios from "axios";
+
 
 const AllOrderList = () => {
   const [data, setData] = useState(null);
