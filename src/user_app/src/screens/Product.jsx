766d0d68d3a4d6bb08ac03c90c47bbd24fde5889
Sugatraj <sugatraj.2106@gmail.com>
2025-02-07 20:08:41 +0530

    menu header show

--- src/user_app/src/screens/Product.jsx
+++ src/user_app/src/screens/Product.jsx
@@ -775,20 +775,24 @@ const Product = () => {
       <Header title="Menu" count={menuList.length} />
 
       <main className={`page-content`}>
-        <div className="container px-3 py-0">
+        {/* <div className="container px-3 py-0">
           <HotelNameAndTable
             restaurantName={restaurantName}
             tableNumber={userData?.tableNumber || "1"}
           />
-        </div>
-        <div className="container px-3 py-0 mb-0">
+        </div> */}
+        {/* <div className="container px-3 py-0 mb-0">
           <HotelNameAndTable
             restaurantName={restaurantName}
             tableNumber={userData?.tableNumber || "1"}
           />
-        </div>
+        </div> */}
         {/* Category Swiper */}
-        <div className="container pb-0 pt-0">
+        <div className="container pb-0 pt-0 p-t50">
+          <HotelNameAndTable
+            restaurantName={restaurantName}
+            tableNumber={userData?.tableNumber || "1"}
+          />
           <div className="d-flex justify-content-between mb-3 pt-1">
             <div className="me-2 w-100" style={{ height: "40px" }}>
               <div
@@ -1123,16 +1127,19 @@ const Product = () => {
                               }}
                             >
                               <i
-                                className={`fa-solid ${
-                                  (() => {
-                                    const storedCart = localStorage.getItem('restaurant_cart_data');
-                                    if (!storedCart) return "fa-plus text-secondary";
-                                    const cartData = JSON.parse(storedCart);
-                                    return cartData.order_items?.some(item => item.menu_id === menu.menu_id)
-                                      ? "fa-circle-check text-success"
-                                      : "fa-plus text-secondary"
-                                  })()
-                                } fs-6`}
+                                className={`fa-solid ${(() => {
+                                  const storedCart = localStorage.getItem(
+                                    "restaurant_cart_data"
+                                  );
+                                  if (!storedCart)
+                                    return "fa-plus text-secondary";
+                                  const cartData = JSON.parse(storedCart);
+                                  return cartData.order_items?.some(
+                                    (item) => item.menu_id === menu.menu_id
+                                  )
+                                    ? "fa-circle-check text-success"
+                                    : "fa-plus text-secondary";
+                                })()} fs-6`}
                               ></i>
                             </div>
                           </div>
@@ -1146,9 +1153,8 @@ const Product = () => {
               <p>No items available with selected filters.</p>
             )}
           </div>
-         
-            <RestaurantSocials />
-         
+
+          <RestaurantSocials />
         </div>
       </main>
 
