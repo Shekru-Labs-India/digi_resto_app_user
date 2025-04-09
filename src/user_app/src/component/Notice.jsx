f3740ecb810b1927001cb676ec30153782e836fe
sugatraj_sarwade <sugatraj.2106@gmail.com>
2025-04-09 23:14:13 +0530

    Refactor Notice component for improved code clarity; adjusted formatting and removed unnecessary comments.

--- src/user_app/src/component/Notice.jsx
+++ src/user_app/src/component/Notice.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function Notice() {
   return (
     <div
-    className="badge bg-primary rounded-0 text-center py-1"
-    style={{ 
-      position: "relative",
-      left: "50%",
-      right: "50%",
-      marginLeft: "-50vw",
-      marginRight: "-50vw",
-      width: "100vw"
-    }}
-  >
-    TESTING
-  </div>
-    // <div></div>
+      className="badge bg-primary rounded-0 text-center"
+      style={{
+        position: "relative",
+        left: "50%",
+        right: "50%",
+        marginLeft: "-50vw",
+        marginRight: "-50vw",
+        width: "100vw",
+      }}
+    >
+      TESTING
+    </div>
   );
 }
 
